3ea4fe50d22b73f1d75967461266f7e10ddd21a6
Christof Leuenberger <christof.leuenberger@mediaparx.ch>
2015-12-03 14:32:17 +0100

    forgot to require CommitAction

--- run.php
+++ run.php
@@ -2,8 +2,6 @@
 
 // @todo
 // - change assigned user on-solved
-// - add config.php for all variable stuff
-// - handling for project name
 // - add "bot" user
 // - add error logging
 
@@ -13,9 +11,9 @@ ini_set('display_errors', 'on');
 require_once 'config.php';
 require_once 'src/BitbucketPostParser.php';
 require_once 'src/ActiveCollabConnector.php';
+require_once 'src/CommitAction.php';
 
 try {
-
 	$input = file_get_contents('data.json');
 
 	// parse incoming data
