c5e1e26e6661c39609b6d1e98df825d42796bbde
Christof Leuenberger <christof.leuenberger@mediaparx.ch>
2015-12-03 14:23:09 +0100

    removed constructor for quick setup. this is already getting messy.

--- src/CommitAction.php
+++ src/CommitAction.php
@@ -6,13 +6,4 @@ class CommitAction
 	public $id;
 	public $link;
 	public $message;
-
-	public function __construct($action, $id, $link, $author, $message = '')
-	{
-		$this->action  = $action;
-		$this->id      = $id;
-		$this->link    = $link;
-		$this->author  = $author;
-		$this->message = $message;
-	}
 }
